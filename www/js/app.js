540eb47e7c9d07ee8ba82db7eb55ef77fdd14913
waldow <walid.alisahraoui@devinci.fr>
2017-02-08 13:55:31 +0100

    Update app.js
    Clean

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,8 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-    angular.module('LURCH', ['ionic','firebase','LURCH.configs'])
+angular.module('LURCH', ['ionic','firebase','LURCH.configs'])
 
 .run(function($ionicPlatform,CONFIG) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
